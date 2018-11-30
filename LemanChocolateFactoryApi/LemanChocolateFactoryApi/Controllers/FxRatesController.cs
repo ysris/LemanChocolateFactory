84dc8f256ad0c7998bada22f8c74d077d19f0ab4
Yoann <yoann@ysris.ch>
2018-11-30 20:08:59 +0100

    HelloWorld

---
+++ LemanChocolateFactoryApi/LemanChocolateFactoryApi/Controllers/FxRatesController.cs
@@ -0,0 +1,21 @@
+﻿using LemanChocolateFactoryApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace LemanChocolateFactoryApi.Controllers
+{
+    [Route("api/rates")]
+    [ApiController]
+    public class FxRatesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<ExchangeRate>> Get()
+        {
+            return new List<ExchangeRate>
+            {
+                new ExchangeRate  { fromCcy="BTC" },
+                new ExchangeRate  { fromCcy="LTC" },
+            };
+        }
+    }
+}
---
+++ LemanChocolateFactoryApi/LemanChocolateFactoryApi/Models/ExchangeRate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemanChocolateFactoryApi.Models
+{
+    public class ExchangeRate
+    {
+        public DateTime? date { get; set; }
+        public string fromCcy { get; set; }
+        public string toCcy { get; set; }
+        public decimal change { get; set; }
+    }
+}
